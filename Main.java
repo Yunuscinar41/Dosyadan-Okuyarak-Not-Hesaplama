20e575e3d62f037f2dcfccc410f0173faf6b6b73
Yunuscinar41 <66219795+Yunuscinar41@users.noreply.github.com>
2020-09-15 17:30:19 +0300

    FileReader-Writer

---
+++ Main.java
@@ -0,0 +1,88 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+    
+    public static String harfnotuHesapla(String isim,int vize1,int vize2,int finalnot){
+        
+        String cikti = "";
+        
+        double toplamnot = (vize1 * 3 / 10.0) + (vize2 * 3 / 10.0) + (finalnot * 4 / 10.0);
+        
+        if(toplamnot >= 90) {
+            
+            cikti = isim + " bu dersten AA aldı...";
+        }
+        else if(toplamnot >= 85) {
+            
+            cikti = isim + " bu dersten BA aldı...";
+        }
+        else if(toplamnot >= 80) {
+            
+            cikti = isim + " bu dersten BB aldı...";
+        }
+        else if(toplamnot >= 75) {
+            
+            cikti = isim + " bu dersten CB aldı...";
+        }
+        else if(toplamnot >= 70) {
+            
+            cikti = isim + " bu dersten CC aldı...";
+        }
+        else if(toplamnot >= 65) {
+            
+            cikti = isim + " bu dersten DC aldı...";
+        }
+        else if(toplamnot >= 60) {
+            
+            cikti = isim + " bu dersten DD aldı...";
+        }
+        else if(toplamnot >= 55) {
+            
+            cikti = isim + " bu dersten FD aldı...";
+        }
+        else{
+            cikti = isim + " bu dersten FF aldı...";
+        }
+        
+        return cikti;
+        
+    }
+    public static void main(String[] args){
+        
+      
+        try(Scanner scanner = new Scanner(new FileReader("dosya.txt"));
+            FileWriter writer = new FileWriter("harfnotu.txt")){
+            
+            while(scanner.hasNextLine()){
+                
+                String ogrenciBilgileri = scanner.nextLine();
+                
+                String[] ogrenciArray = ogrenciBilgileri.split(",");
+                // split metodu stringi parçalara ayırır parantez içine yazdığımız şeye göre.
+                
+                int vize1 = Integer.valueOf(ogrenciArray[1]);
+                int vize2 = Integer.valueOf(ogrenciArray[2]);
+                int finalnot = Integer.valueOf(ogrenciArray[3]);
+                
+                String cikti = harfnotuHesapla(ogrenciBilgileri, vize1, vize2, finalnot);
+                
+                writer.write(cikti + "\n");
+                
+            }
+            
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("Dosya bulunamadı...");
+        } catch (IOException ex) {
+            System.out.println("Dosya açılırken bir hata oluştu...");
+        }
+    }
+}
